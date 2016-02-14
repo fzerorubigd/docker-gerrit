bd40be2eca08aeb1679543594a4f38b25b336294
fzerorubigd <fzerorubigd@gmail.com>
2016-02-14 22:55:03 +0330

    disable auto start on init

--- docker-initscript.sh
+++ docker-initscript.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if [ "$1" = 'gerrit' ]; then
     # Run init on the data directory
-    java -jar ${GERRIT_WAR} init --batch -d /home/gerrit/data
+    java -jar ${GERRIT_WAR} init --batch --no-auto-start -d /home/gerrit/data
     # supervise gerrit 
     /bin/bash /home/gerrit/data/bin/gerrit.sh supervise
 else
